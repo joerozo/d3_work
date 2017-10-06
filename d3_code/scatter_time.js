c496c04461f7180ae5a9c9068415d261520200b7
joerozo <joe.rozo@productops.com>
2017-10-05 18:14:36 -0700

    finished ts analysis

--- d3_code/scatter_time.js
+++ d3_code/scatter_time.js
@@ -21,6 +21,68 @@ function gc_line_mouseOut(){
 	gc_line.attr("stroke-width", "2.5")
 }
 
+function blue_circle_mouseOver(){
+	blue_circle_var.attr("r", "7.5")
+}
+
+function blue_circle_mouseOut(){
+	blue_circle_var.attr("r", "5")
+}
+
+function teal_circle_mouseOver(){
+	teal_circle_var.attr("r", "7.5")
+}
+
+function teal_circle_mouseOut(){
+	teal_circle_var.attr("r", "5")
+}
+
+function grey_circle_mouseOver(){
+	grey_circle_var.attr("r", "7.5")
+}
+
+function grey_circle_mouseOut(){
+	grey_circle_var.attr("r", "5")
+}
+
+function black_circle_mouseOver(){
+	black_circle_var.attr("r", "7.5")
+}
+
+function black_circle_mouseOut(){
+	black_circle_var.attr("r", "5")
+}
+
+function blueCircleClick(){
+	var active = bc.active ? false : true,
+	   newOpacity = active ? 0 : 1;
+	   ts_var.attr("opacity", newOpacity);
+	   bc.active = active;
+}
+
+function tealCircleClick(){
+	var active = gc.active ? false : true,
+	   newOpacity = active ? 0 : 1;
+	   gc_var.attr("opacity", newOpacity);
+	   gc.active = active;
+}
+/*
+ on click var active is either true or false
+ if false 
+*/
+function greyCircleClick(){
+	var active = gc.active ? false : true,
+       newOpacity = active ? 0 : 1;
+       gc_line.attr("opacity", newOpacity);
+       gc.active = active;
+}
+
+function blackCircleClick(){
+	var active = ts.active ? false : true,
+       newOpacity = active ? 0 : 1;
+       ts_line.attr("opacity", newOpacity);
+       ts.active = active;
+}
 /******** Initialization Vars *********/
 
 d3.select("body")
@@ -35,6 +97,7 @@ var ts_var = d3.select("svg")
     .data(data.times_square)
     .enter()
     .append("circle")
+      .attr("id", "bc")
       .attr("class", "times_square")
       .attr("fill", "#3423ed")
     .on("mouseover", ts_line_mouseOver)
@@ -45,10 +108,19 @@ var gc_var = d3.select("svg")
     .data(data.grand_central)
     .enter()
     .append("circle")
+      .attr("id", "gc")
       .attr("class", "grand_central")
       .attr("fill", "#24c8e5")
     .on("mouseover", gc_line_mouseOver)
 	.on("mouseout", gc_line_mouseOut)
+
+// d3.select("svg")
+//     .append("circle")
+//       .attr("class", "grand_central")
+//       .attr("fill", "#24c8e5")
+//       .attr("transform", "translate(" + margin + ",180)")
+//     .on("mouseover", gc_line_mouseOver)
+// 	.on("mouseout", gc_line_mouseOut)
 
 var line = d3.line()
 	.x(function(d){
@@ -123,8 +195,45 @@ d3.select("svg")
     .attr("transform", "translate(280, 20)")
     .text("Turnstile Time Analysis")
 
+var blue_circle_var = d3.select("svg")
+  .append("circle")
+      .attr("fill", "#3423ed")
+      .attr("r", "5")
+   	  .attr("transform", "translate(480, 290)")
+  .on("mouseover", blue_circle_mouseOver)
+  .on("mouseout", blue_circle_mouseOut)
+  .on("click", blueCircleClick)
+
+var teal_circle_var = d3.select("svg")
+  .append("circle")
+      .attr("fill", "#24c8e5")
+      .attr("r", "5")
+   	  .attr("transform", "translate(510, 290)")
+  .on("click", tealCircleClick)
+  .on("mouseover", teal_circle_mouseOver)
+  .on("mouseout", teal_circle_mouseOut)
+
+var grey_circle_var = d3.select("svg")
+  .append("circle")
+      .attr("fill", "Grey")
+      .attr("r", "5")
+   	  .attr("transform", "translate(540, 290)")
+  .on("click", greyCircleClick)
+  .on("mouseover", grey_circle_mouseOver)
+  .on("mouseout", grey_circle_mouseOut)
+
+var black_circle_var = d3.select("svg")
+  .append("circle")
+      .attr("fill", "Black")
+      .attr("r", "5")
+   	  .attr("transform", "translate(570, 290)")
+  .on("click", blackCircleClick)
+  .on("mouseover", black_circle_mouseOver)
+  .on("mouseout", black_circle_mouseOut)
+
 var ts_line = d3.select("svg")
 	.append("path")
+	.attr("id", "ts")
 	.attr("d", line(data.times_square))
 	.attr("fill", "none")
 	.attr("stroke", "Black")
@@ -132,10 +241,9 @@ var ts_line = d3.select("svg")
 	.on("mouseover", ts_line_mouseOver)
 	.on("mouseout", ts_line_mouseOut);
 
-
-
 var gc_line = d3.select("svg")
 	.append("path")
+	.attr("id", "gc")
 	.attr("d", line(data.grand_central))
 	.attr("fill", "none")
 	.attr("stroke", "Grey")
